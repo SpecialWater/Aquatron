ae3e3cfa97b1fba19fe9721041858f3765a7fab4
James Pruet <andrew_pruet@oxy.com>
2020-11-24 14:40:48 -0600

    connected to local API

--- angular-Aquatron/src/app/settings.service.ts
+++ angular-Aquatron/src/app/settings.service.ts
@@ -1,16 +1,29 @@
 import { Injectable } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 
 @Injectable({
   providedIn: 'root'
 })
+
 export class SettingsService {
+
+  headers = new HttpHeaders({
+    'Content-Type': 'application/json'
+  });
 
   // very important that JSON is formated in "record" format (see pandas to_json doc)
   getSettings() {
     // console.log('https://localhost:5001/api' + current_route)
     //return this.http.get('https://localhost:5001/api' + current_route)
     return this.http.get('http://localhost:5001/settings/get')
+  }
+
+  postSettings(data){
+    return this.http.post<any>('http://localhost:5001/settings/post',
+      JSON.stringify(data),
+      {headers: this.headers}).subscribe(
+        // data => {console.log(data)}
+      );
   }
 
 
