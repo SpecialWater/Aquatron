6d47d77d47d8850e16017bd0e45d38851a238f38
james_pruet@oxy.com <james_pruet@oxy.com>
2021-01-04 11:38:09 -0600

    update flask for angular charting

--- FlaskApp/app/database.py
+++ FlaskApp/app/database.py
@@ -1,6 +1,7 @@
 from azure.cosmos import exceptions, CosmosClient, PartitionKey
 from app.config import Config
 
+
 class Database:
     endpoint = Config.cosmosURL
     key = Config.cosmosKey
@@ -8,17 +9,18 @@ class Database:
     database_name = Config.cosmosDB
     database = client.get_database_client(database_name)
 
+
 class Client(Database):
-    
+
     def __init__(self, containerName):
         self._name = containerName
         self._container = self.database.get_container_client(self._name)
-        
+
     def _get_container(self):
         return self._container
-    
+
     def _set_container(self):
         self._container = self.database.get_container_client(self._name)
-    
+
     # Allows class.container to return the container
     container = property(_get_container, _set_container)
--- FlaskApp/app/utility.py
+++ FlaskApp/app/utility.py
@@ -3,16 +3,18 @@ import pytz
 from azure.iot.hub import IoTHubRegistryManager
 from app.config import Config
 
+
 def getID():
-    dt = datetime.now(pytz.timezone('US/Central'))
-    id = f'{dt:%Y}{dt:%m}{dt:%d}{dt:%H}{dt:%M}{dt:%S}'
-        
+    dt = datetime.now(pytz.timezone('Etc/UTC'))
+    id = f'{dt:%Y}-{dt:%m}-{dt:%d}T{dt:%H}:{dt:%M}:{dt:%S}.0000000Z'
+
     return id
 
+
 def getPartition():
-    dt = datetime.now(pytz.timezone('US/Central'))
-    partition = f'{dt:%Y}{dt:%m}{dt:%d}'
-        
+    dt = datetime.now(pytz.timezone('Etc/UTC'))
+    partition = f'{dt:%Y}-{dt:%m}-{dt:%d}T00:00:00.0000000Z'
+
     return partition
 
 
@@ -21,7 +23,7 @@ def send_C2D_message():
     device_id = Config.IOTHUB_DEVICE_ID
     registry_manager = IoTHubRegistryManager(conn_string)
     data = "New Settings Saved!"
-    
+
     registry_manager.send_c2d_message(
         device_id,
         data)
