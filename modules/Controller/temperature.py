4e185aa68ace5a3b40e3fd6240b7ee04e08cbc89
Andrew Pruet <japruet@utexas.edu>
2021-01-05 16:19:20 -0600

    temperature cleanup

--- modules/Controller/temperature.py
+++ modules/Controller/temperature.py
@@ -1,17 +1,10 @@
-import os
-import datetime
-import asyncio
-import json
 import time
-# from dotenv import load_dotenv
 from azure.iot.device.aio import IoTHubModuleClient
 from w1thermsensor import W1ThermSensor
 
 
 class Temperature:
     def __init__(self):
-        # load_dotenv()
-
         while True:
                 try:
                     self.sensor = W1ThermSensor()
